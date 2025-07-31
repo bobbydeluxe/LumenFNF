ef5bd074715c8048734d5256432d85d8fcac4f0a
bobbyDX <veersparekh@outlook.com>
2025-07-30 20:28:12 -0400

    fix the fade shader and do some stuff with the setup

--- source/shaders/BlueFade.hx
+++ source/shaders/BlueFade.hx
@@ -1,51 +1,63 @@
 package shaders;
 
 import flixel.system.FlxAssets.FlxShader;
+import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 
 class BlueFade extends FlxShader
 {
-    public var fadeVal(default, set):Float;
-    public var fadeRGB(default, set):Array<Float>; // [r,g,b]
+  public var fadeVal(default, set):Float;
+  public var fadeRGB(default, set):Array<Float>; // [r,g,b] multipliers
 
-    function set_fadeVal(val:Float):Float {
-        fadeAmt.value = [val];
-        fadeVal = val;
-        return val;
-    }
+  function set_fadeVal(val:Float):Float
+  {
+    fadeAmt.value = [val];
+    fadeVal = val;
+    return val;
+  }
 
-    function set_fadeRGB(val:Array<Float>):Array<Float> {
-        if (val.length < 3) throw "fadeRGB must be [r,g,b]";
-        fadeColor.value = [val[0], val[1], val[2]];
-        fadeRGB = val;
-        return val;
-    }
+  function set_fadeRGB(val:Array<Float>):Array<Float>
+  {
+    if (val.length < 3) throw "fadeRGB must be [r,g,b]";
+    fadeR.value = [val[0]];
+    fadeG.value = [val[1]];
+    fadeB.value = [val[2]];
+    fadeRGB = val;
+    return val;
+  }
 
-    public function fade(startAmt:Float = 0, targetAmt:Float = 1, duration:Float, _options:TweenOptions):Void {
-        fadeVal = startAmt;
-        FlxTween.tween(this, {fadeVal: targetAmt}, duration, _options);
-    }
+  public function fade(startAmt:Float = 0, targetAmt:Float = 1, duration:Float, _options:TweenOptions):Void
+  {
+    fadeVal = startAmt;
+    FlxTween.tween(this, {fadeVal: targetAmt}, duration, _options);
+  }
 
-    @:glFragmentSource('
+  @:glFragmentSource('
         #pragma header
 
         uniform float fadeAmt;
-        uniform vec3 fadeColor; // New RGB control
+        uniform float fadeR;
+        uniform float fadeG;
+        uniform float fadeB;
 
         void main()
         {
-            vec4 tex = flixel_texture2D(bitmap, openfl_TextureCoordv);
+          vec4 tex = flixel_texture2D(bitmap, openfl_TextureCoordv);
 
-            vec4 colorTarget = vec4(fadeColor.rgb, tex.a);
-            vec4 finalColor = mix(colorTarget * fadeAmt, tex * fadeAmt, fadeAmt);
+          // use customizable RGB instead of hardcoded 0.0, 0.0, tex.b
+          vec4 finalColor = mix(
+            vec4(vec4(tex.r * fadeR, tex.g * fadeG, tex.b * fadeB, tex.a) * fadeAmt),
+            vec4(tex * fadeAmt),
+            fadeAmt
+          );
 
-            gl_FragColor = finalColor;
+          gl_FragColor = finalColor;
         }
     ')
-    public function new()
-    {
-        super();
-        this.fadeVal = 1;
-        this.fadeRGB = [0.0, 0.0, 1.0]; // Default to blue
-    }
+  public function new()
+  {
+    super();
+    this.fadeVal = 1;
+    this.fadeRGB = [0.0, 0.0, 1.0]; // default blue effect
+  }
 }
